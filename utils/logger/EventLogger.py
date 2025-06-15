bac34d131e94c0403290928694a842f1aeb4d96d
Ajay C Hiremath <95692568+AjayCHiremath@users.noreply.github.com>
2025-06-15 17:28:32 +0100

    Update EventLogger.py

--- utils/logger/EventLogger.py
+++ utils/logger/EventLogger.py
@@ -9,11 +9,11 @@ def log_message(message: str, log_file:str, echo=False):
     log_file += f"{session_id[0]}/{session_id[1]}_logs.txt".replace("//", "/")
 
     # Read the existing log file from S3
-    file = read_auth_file_from_s3(bucket_name=os.getenv("MY_S3_BUCKET"), object_key=log_file, use_locally=False)
+    file = read_auth_file_from_s3(bucket_name=os.getenv("MY_S3_BUCKET"), object_key=log_file, use_locally=True)
     # If the file is empty, initialize it as a list
     file.append(message + "\n")
     # Write the updated log file back to S3
-    write_auth_file_to_s3(authorized_user_data=file, bucket_name=os.getenv("MY_S3_BUCKET"), object_key=log_file, use_locally=False)
+    write_auth_file_to_s3(authorized_user_data=file, bucket_name=os.getenv("MY_S3_BUCKET"), object_key=log_file, use_locally=True)
 
     if echo:
         print(message)
